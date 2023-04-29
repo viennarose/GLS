35fed02a4fe62d6084a59db3cbe23c58c2ffb2ac
viennarose <viennapepito01@gmail.com>
2023-04-29 18:22:53 +0800

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.3/umd/popper.min.js"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-     @livewireStyles
+     @livewireStyles()
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -94,5 +94,5 @@
     </div>
 
 </body>
-@livewireScripts
+@livewireScripts()
 </html>
