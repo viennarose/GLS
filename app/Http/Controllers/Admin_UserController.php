8117432712e996e3a96fcb485e906f9e2f15850d
Kenn Secusana <kennsecusana@gmail.com>
2023-05-01 14:12:13 +0800

    Notify User When Approved

--- app/Http/Controllers/Admin_UserController.php
+++ app/Http/Controllers/Admin_UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Notifications\UserApprovedNotification;
+use App\Notifications\NewUser;
+
 
 class Admin_UserController extends Controller
 {
@@ -19,6 +22,8 @@ class Admin_UserController extends Controller
         $user = User::findOrFail($user_id);
         $user->update(['approved_at' => now()]);
 
+        $user->notify(new UserApprovedNotification());
+        
         return redirect()->route('admin.users.index')->with('success_message', 'Request Approved successfully');
     }
 
---
+++ app/Notifications/UserApprovedNotification.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserApprovedNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+  /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Your account has been approved')
+                    ->line('Your account has been approved by the admin.')
+                    ->action('Go to Login', url('/login'))
+                    ->line('Thank you for using our application!');
+    }
+}
+
+
+
+
+
