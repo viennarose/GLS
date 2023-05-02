b8425684cd0576fd7677be4a20f1d4188a76c43a
Kenn Secusana <kennsecusana@gmail.com>
2023-05-02 09:59:02 +0800

    Finished Re-send Approval Req to Admin

--- app/Http/Controllers/Admin_UserController.php
+++ app/Http/Controllers/Admin_UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Notifications\UserApprovedNotification;
 use App\Notifications\NewUser;
+use Notification;
 
 
 class Admin_UserController extends Controller
@@ -38,4 +39,20 @@ class Admin_UserController extends Controller
     return redirect()->back()->with('delete_message', 'Request Deleted');
 
     }
+
+    public function resendApprovalNotification(Request $request, $user_id)
+   {
+    $user = User::findOrFail($user_id);
+
+    if ($user->approved_at) {
+    return redirect()->back()->withErrors(['User has already been approved.']);
+   }
+
+    $admin_email = User::where('admin', '1')->value('email');
+    Notification::send(User::where('email', $admin_email)->first(), new NewUser($user));
+
+    return redirect()->back()->with('success', 'Approval notification has been resent to the admin.');
+
+  }
+
 }
