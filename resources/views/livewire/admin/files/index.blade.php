921b80ea12f2adb8339e43fdd5ec2735007d2da7
viennarose <viennapepito01@gmail.com>
2023-05-04 10:59:36 +0800

    vienna

--- resources/views/livewire/admin/files/index.blade.php
+++ resources/views/livewire/admin/files/index.blade.php
@@ -113,7 +113,7 @@
                                             <div class="modal-dialog modal-md">
                                                 <div class="modal-content">
                                                     <div class="modal-header" style="background-color: #234495; color:white;">
-                                                        <h5 class="modal-title" id="updateFileLabel">Adding EDICTS</h5>
+                                                        <h5 class="modal-title" id="updateFileLabel">Updating EDICTS</h5>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
